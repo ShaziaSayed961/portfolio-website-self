c29d2162149f3074e565da8374c76dd1678c2fd5
ShaziaSayed961 <121424478+ShaziaSayed961@users.noreply.github.com>
2026-07-04 02:38:25 +0530

    added about section

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
+// const spotlight = document.querySelector(".spotlight");
+
+// window.addEventListener("mousemove",(e)=>{
+
+// spotlight.style.background =
+// `radial-gradient(
+// circle 250px at ${e.clientX}px ${e.clientY}px,
+
+// rgba(200, 217, 116, 0.15),
+
+// transparent 80%
+// )`;
+
+// });
+
+//  rgba(100,255,218,.15),
+
+
 const spotlight = document.querySelector(".spotlight");
 
-window.addEventListener("mousemove",(e)=>{
-
-spotlight.style.background =
-`radial-gradient(
-circle 250px at ${e.clientX}px ${e.clientY}px,
-
-rgba(200, 217, 116, 0.15),
-
-transparent 80%
-)`;
-
+window.addEventListener("mousemove", (e) => {
+  spotlight.style.background = `
+    radial-gradient(
+      circle 350px at ${e.clientX}px ${e.clientY}px,
+      rgba(255,255,255,0.08),
+      rgba(255,255,255,0.03) 35%,
+      transparent 75%
+    )
+  `;
 });
-
- rgba(100,255,218,.15),
